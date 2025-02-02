556889501641a6ae2990c10a6a43288f310c9014
pareekakhilesh123 <pareekakhilesh5@gmail.com>
2025-02-02 12:25:09 +0530

    post api create

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,35 +1,17 @@
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
 const router = express.Router();
 
-const ordersFilePath = path.join(__dirname, "../db/order.json");
- 
-const readOrders = () => {
-    try {
-        const data = fs.readFileSync(ordersFilePath, "utf-8");
-        return JSON.parse(data);
-    } catch (error) {
-        return [];
-    }
-};
+let orders = []; 
 
- 
-const writeOrders = (orders) => {
-    fs.writeFileSync(ordersFilePath, JSON.stringify(orders, null, 2), "utf-8");
-};
- 
+
 router.get("/orders", (req, res) => {
-    const orders = readOrders();
     res.json(orders);
 });
-
  
-router.post("/orders", (req, res) => {
-    const orders = readOrders();
 
+router.post("/orders", (req, res) => {
     const newOrder = {
-        id: orders.length + 1,  
+        id: orders.length + 1,
         userId: req.body.userId,
         items: req.body.items,
         totalAmount: req.body.totalAmount,
@@ -37,8 +19,6 @@ router.post("/orders", (req, res) => {
     };
 
     orders.push(newOrder);
-    writeOrders(orders);  
-
     res.status(201).json({
         message: "Order added successfully",
         order: newOrder
